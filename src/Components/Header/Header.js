070578836df6de7ce8472e1ca5e5c86bfaf9537a
Prakash Chandra Bhatt <77747508+prakashbhattcse@users.noreply.github.com>
2023-09-09 01:15:07 +0530

    login_logout

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React , {useState} from 'react'
 import "./Header.css"
 import { LOGO_URL } from '../../utils/constants'
 
 const Header = () => {
+
+    const [login, setLogin] = useState(false)
     return (
         <div className="header">
             <div className='logo-container'>
@@ -15,6 +17,7 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button onClick={()=>{setLogin(!login)}}>{login ? "Login" : "Logout"}</button>
                 </ul>
             </div>
         </div>
