500c98715aff817decf95e2b6f13d534aec6df28
Prakash Chandra Bhatt <77747508+prakashbhattcse@users.noreply.github.com>
2024-08-06 20:33:52 +0530

    Update Shimmer.js

--- src/Components/Shimmer/Shimmer.js
+++ src/Components/Shimmer/Shimmer.js
@@ -1,33 +1,6 @@
 import React from 'react'
 import "./Shimmer.css"
 
-
-// const Shimmer = () => {
-//     return (
-//         <div className="shimmer-container">
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//             <div className="shimmer-card"></div>
-//         </div>
-//     )
-// }
-
-// export default Shimmer
-
-
-
 const Shimmer = () => {
     return (
         <div className="shimmer-container">
